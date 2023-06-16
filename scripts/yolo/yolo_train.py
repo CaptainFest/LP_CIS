11eb0af51d60266b415d993dc80d90e1893f0c2c
CaptainFest <xanilegendx@gmail.com>
2023-06-16 09:32:55 +0300

    yolo v5 support

--- scripts/yolo/yolo_train.py
+++ scripts/yolo/yolo_train.py
@@ -11,15 +11,17 @@ def parse_args():
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--aug', action=argparse.BooleanOptionalAction)
     parser.add_argument('--model_size', type=str, default='s', choices=['n', 's', 'm'])
+    parser.add_argument('--model_version', type=int, default=8, choices=[5, 8])
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    model = YOLO(f"yolov8{args.model_size}.pt")
+    model = YOLO(f"yolov{args.model_version}{args.model_size}.pt")
     model.to(args.device)
-    model.train(data=args.data_folder, batch=args.batch, imgsz=640, epochs=args.epochs,
-                augment=args.aug, name=f'yolov8{args.model_size}_b{args.batch}_ep{args.epochs}',
+    aug_n = '_aug' if args.aug else ''
+    model.train(data=args.data_folder, batch=args.batch, imgsz=640, epochs=args.epochs, augment=args.aug,
+                name=f'yolov{args.model_version}{args.model_size}{aug_n}_b{args.batch}_ep{args.epochs}',
                 project='../../exps/runs')
-    model.val(name=f'yolov8{args.model_size}_b{args.batch}_ep{args.epochs}_val')
+    model.val(name=f'yolov{args.model_version}{args.model_size}{aug_n}_b{args.batch}_ep{args.epochs}_val')
